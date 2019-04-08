3c218adb60a7a3a766a016693c5312c86d13cba9
Alfiya1998 <alfiya1998.mansurova@yandex.ru>
2019-04-08 17:34:39 +0500

    update2

--- moviebot.py
+++ moviebot.py
@@ -18,14 +18,60 @@ def get_new_movie():
     movie_name = parser.xpath('.//div[@class = "films_right"]/a/span/span/text()')
 
     # Получение списка жанров фильмов
-    movie_info = parser.xpath('.//span[@class = "films_info"]/text()')
+    movie_genre = parser.xpath('.//span[@class = "films_info"]/text()')
 
     # Получение списка ссылок на фильмы
     movie_link = parser.xpath('.//div[@class = "films_right"]/a/@href')
 
+    movie_info = parser.xpath('.//div[@class = "showtimes_cell"]')
+
+    movie_format = []
+    movie_time = []
+    movie_price = []
+    for i in range(len(movie_info)):
+        if (i % 2 != 0 and i > 3):
+
+            format = []
+            time = []
+            temp_time = []
+            price = []
+            temp_price = []
+            flag = 1
+
+            for child in movie_info[i].iter():
+                if child.get('class') == "showtimes_format":
+                    if (flag == 0):
+                        time.append(temp_time)
+                        price.append(temp_price)
+                        temp_time = []
+                        temp_price = []
+                    format.append(child.text)
+                    flag = 0
+                if child.get('class') == "session_time":
+                    temp_time.append(child.text)
+                if child.get('class') == "session_price":
+                    temp_price.append(child.text)
+
+            time.append(temp_time)
+            price.append(temp_price)
+
+            movie_format.append(format)
+            movie_time.append(time)
+            movie_price.append(price)
+
     mystr = ""
+
     for i in range(len(movie_name)):
-        mystr = mystr + movie_name[i] + "\n" + "Жанр: " + movie_info[i] + "\n" + "Ссылка: " + movie_link[i] + "\n\n"
+        mystr = mystr + movie_name[i] + "\n" + "Жанр: " + movie_genre[i] + "\n"
+        for j in range(len(movie_format[i])):
+            mystr = mystr + movie_format[i][j] + ": "
+            for k in range(len(movie_time[i][j])):
+                try:
+                    mystr = mystr + movie_time[i][j][k] + "(" + movie_price[i][j][k] + ") "
+                except:
+                    mystr = mystr + movie_time[i][j][k] + "(цена не указана) "
+            mystr = mystr + "\n"
+        mystr = mystr + "Ссылка: " + movie_link[i] + "\n\n"
 
     return mystr
 
