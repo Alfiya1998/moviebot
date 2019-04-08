16c7be99fdca62855fb3f17bf12a1515537bda6c
Alfiya1998 <alfiya1998.mansurova@yandex.ru>
2019-04-08 17:12:45 +0500

    First2

--- moviebot.py
+++ moviebot.py
@@ -1,4 +1,4 @@
-import vk_api
+п»їimport vk_api
 import random
 import lxml.html
 import requests
@@ -14,53 +14,53 @@ def get_header_movie():
     page = requests.get('https://perm.kinoafisha.info/cinema/4283797/').text
     parser = lxml.html.fromstring(page)
 
-    # Название фильма
+    # ГЌГ Г§ГўГ Г­ГЁГҐ ГґГЁГ«ГјГ¬Г 
     path_movie_name = ".films_right > a > span > span"
     movie_name = []
     for i in parser.cssselect(path_movie_name):
         movie_name.append(i.text)
 
-    # Жанр фильма
+    # Г†Г Г­Г° ГґГЁГ«ГјГ¬Г 
     path_movie_info = ".films_info"
     movie_info = []
     for i in parser.cssselect(path_movie_info):
         movie_info.append(i.text)
 
-    # Ссылка на фильм
+    # Г‘Г±Г»Г«ГЄГ  Г­Г  ГґГЁГ«ГјГ¬
     movie_link = parser.xpath('.//div[@class = "films_right"]/a/@href')
 
     mystr = ""
     for i in range(len(movie_name)):
-        mystr = mystr + movie_name[i] + "\n" + "Жанр: " + movie_info[i] + "\n" + "Ссылка: " + movie_link[i] + "\n\n"
+        mystr = mystr + movie_name[i] + "\n" + "Г†Г Г­Г°: " + movie_info[i] + "\n" + "Г‘Г±Г»Г«ГЄГ : " + movie_link[i] + "\n\n"
 
     return mystr
 
 def get_random_id():
     return random.getrandbits(31) * random.choice([-1, 1])
 
-# API-ключ созданный ранее
+# API-ГЄГ«ГѕГ· Г±Г®Г§Г¤Г Г­Г­Г»Г© Г°Г Г­ГҐГҐ
 token = "9bf9a118f6935e61fdc7e437c97a3f767663719de2747b6ba29a6477a86e488b1fa8a7116514c4cb1c616"
 
-# Авторизуемся как сообщество
+# ГЂГўГІГ®Г°ГЁГ§ГіГҐГ¬Г±Гї ГЄГ ГЄ Г±Г®Г®ГЎГ№ГҐГ±ГІГўГ®
 vk_session = vk_api.VkApi(token=token)
 
 longpoll = VkLongPoll(vk_session)
 
 for event in longpoll.listen():
 
-    # Если пришло новое сообщение
+    # Г…Г±Г«ГЁ ГЇГ°ГЁГёГ«Г® Г­Г®ГўГ®ГҐ Г±Г®Г®ГЎГ№ГҐГ­ГЁГҐ
     if event.type == VkEventType.MESSAGE_NEW:
 
-        # Если оно имеет метку для меня( то есть бота)
+        # Г…Г±Г«ГЁ Г®Г­Г® ГЁГ¬ГҐГҐГІ Г¬ГҐГІГЄГі Г¤Г«Гї Г¬ГҐГ­Гї( ГІГ® ГҐГ±ГІГј ГЎГ®ГІГ )
         if event.to_me:
 
-            # Сообщение от пользователя
+            # Г‘Г®Г®ГЎГ№ГҐГ­ГЁГҐ Г®ГІ ГЇГ®Г«ГјГ§Г®ГўГ ГІГҐГ«Гї
             request = event.text
 
-            # Логика ответа
-            if request == "привет":
-                write_msg(event.user_id, "Привет")
-            elif request == "новинки":
+            # Г‹Г®ГЈГЁГЄГ  Г®ГІГўГҐГІГ 
+            if request == "ГЇГ°ГЁГўГҐГІ":
+                write_msg(event.user_id, "ГЏГ°ГЁГўГҐГІ")
+            elif request == "Г­Г®ГўГЁГ­ГЄГЁ":
                 write_header_movie(event.user_id)
             else:
-                write_msg(event.user_id, "Я вас не понимаю(((")
+                write_msg(event.user_id, "Гџ ГўГ Г± Г­ГҐ ГЇГ®Г­ГЁГ¬Г Гѕ(((")
