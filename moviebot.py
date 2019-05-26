4016ed7bbb03c2fa6cb2746a7f533ea8eaa7b8c3
AnyaPashkovskaya <mayamis99@mail.ru>
2019-05-26 17:09:39 +0500

    Add find genre

--- moviebot.py
+++ moviebot.py
@@ -3,8 +3,50 @@ import random
 import lxml.html
 import requests
 import json
+import re
 from vk_api import VkUpload
 from vk_api.longpoll import VkLongPoll, VkEventType
+
+my_genre = ["биография","военный", 'ужасы','семейный', 'триллер', 'комедия','боевик', 'фантастика', 'фэнтези',"приключение","мелодрама" , "драма"]
+genre_msg = ""
+for i in my_genre:
+  genre_msg += i + "\n"
+
+def Find(listword):
+  wordlist=re.sub("[^\w]", " ", listword).split()
+  result  = list(set(wordlist) & set(my_genre))
+  return result
+
+def parser_listgenre(word):
+    page = requests.get('https://perm.kinoafisha.info/cinema/4283797/').text
+    parser = lxml.html.fromstring(page)
+    # название фильма
+    movie_name = parser.xpath('.//div[@class = "films_right"]/a/span/span/text()')
+    # жанр фильма
+    movie_genre = parser.xpath('.//span[@class = "films_info"]/text()')
+
+    return_movie = []
+    print(movie_genre)
+    for i in movie_genre:
+        print("=====",i,"====")
+        temp = list(i.split(', '))
+        print(temp)
+        print((i))
+        result = list(set(temp) & set(word))
+        print(result)
+        if(len(result)==len(word)):
+            index = movie_genre.index(i)
+            return_movie.append(movie_name[index] + " [" + movie_genre[index] + "]\n")
+
+
+    print(return_movie)
+    return_movie = set(return_movie)
+    str = ""
+    for i in return_movie:
+        str+= i + "\n"
+    if str=="":
+        str = "Я не смог найти фильмы с данным жанром";
+    return str
 
 keyboard = {
     "one_time": None,
@@ -49,6 +91,9 @@ def write_msg(user_id, message):
 
 def write_new_movie(user_id, keyboard, cinema_id):
     vk_session.method("messages.send", {"user_id": user_id, "message": get_new_movie(cinema_id), "random_id": get_random_id(), "keyboard": keyboard})
+
+def write_recomendet(user_id, genre):
+	vk_session.method("messages.send", {"user_id": user_id, "message": parser_listgenre(genre), "random_id": get_random_id()})
 
 def get_new_movie(cinema_id):
     page = requests.get('https://perm.kinoafisha.info/cinema/' + cinema_id).text
@@ -119,7 +164,7 @@ def get_random_id():
     return random.getrandbits(31) * random.choice([-1, 1])
 
 # API-ключ созданный ранее
-token = "e132b79daa44a0025aa2e72e484a1f3ddd18f8f6d685ed2a4efa1b4eb37685aab33746c1bb42b5b7dffc8"
+token = "19b0bec16f37a5ef6754988c36a9759c11a92e4d3a2f6210c2809ab6088921f92b99188cb9d028d7b1695"
 
 # Авторизуемся как сообщество
 vk_session = vk_api.VkApi(token=token)
@@ -135,19 +180,18 @@ for event in longpoll.listen():
 
     # Если пришло новое сообщение и если оно имеет метку для меня( то есть бота)
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-
-<<<<<<< HEAD
-        # Сообщение от пользователя или текст кнопки
         request = event.text.lower()
-=======
-            # Сообщение от пользователя
-            request = event.text.lower()
->>>>>>> d25fc371ee81e94d10a1524a5b93bfe6bf576b3e
 
         # Логика ответа
         if request == "привет":
             write_msg(event.user_id, "Привет")
         elif request == "колизей" or request == "семья" or request == "кристалл":
             write_new_movie(event.user_id, json.dumps(keyboard, ensure_ascii=False), cinema[request])
+        elif request == "поиск по жанру":
+        	write_msg(event.user_id, "Для поиска фильма по жанру напишите один из перечисленных жанров:\n{0}".format(genre_msg))
+        elif Find(request):
+                listtem = Find(request)
+                print(listtem)
+                write_recomendet(event.user_id, listtem)
         else:
             write_msg(event.user_id, "Я вас не понимаю(((")
